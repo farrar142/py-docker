4f6f0d09d6262c3e38cab85d9fd280fa818295f0
farrar142 <gksdjf1690@gmail.com>
2023-03-03 19:52:03 +0000

    fix:배포 명령어 수정

--- celery.sh
+++ celery.sh
@@ -1,5 +1,5 @@
 # celery multi stopwait 2 -A runthe_backend --pidfile=/var/run/auth%n.pid --logfile=/var/run/auth%n.log --loglevel=INFO
-celery multi start 2 -A base --logfile=/usr/src/app/logs/$1_%n.log --loglevel=INFO --max-tasks-per-child=1 --concurrency=2
+celery multi start 2 -A base --logfile=/usr/src/app/logs/$1_%n.log --loglevel=INFO --max-tasks-per-child=1 --concurrency=1
 # celery multi start 2 -A runthe_backend --logfile=/var/run/auth%n.log --loglevel=INFO 
-celery -A base worker --beat --scheduler django --max-tasks-per-child=1 --concurrency=2 --loglevel=info --logfile=/usr/src/app/logs/$1_celerybeat.log -E -n master@node
+celery -A base worker --beat --scheduler django --max-tasks-per-child=1 --concurrency=1 --loglevel=info --logfile=/usr/src/app/logs/$1_celerybeat.log -E -n master@node
 # rm -rf /var/run/auth_celerybeat.pid
